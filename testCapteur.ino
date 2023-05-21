6550cfeec6b188b0385bcdc72cf88ab560eddb2b
Well-John0611 <119621370+Well-John0611@users.noreply.github.com>
2023-05-21 13:28:24 +0200

    Update testCapteur.ino

--- testCapteur.ino
+++ testCapteur.ino
@@ -1,12 +1,12 @@
-#include CapteurCardiaque.h
+#include "CapteurCardiaque.h"
 
 void setup() {
   int a;
   // put your setup code here, to run once:
   CapteurCardiaque Capt_C(D7,D3); //entree sur D7 et sortie sur D3
-  Capt_C.calculateHeartBeat();//calcul de la frequence
-  a=Capt_C.getHeartBeat();
-  Capt_C.sendHeartBeat();
+  Capt_C.calculateHeartbeat();//calcul de la frequence
+  a=Capt_C.getHeartbeat();
+  Capt_C.sendHeartbeat();
   
 
 }
