b0b4651173a6ca53176978accc3e7f0e84140fe4
Well-John0611 <119621370+Well-John0611@users.noreply.github.com>
2023-05-24 00:35:55 +0200

    Update CapteurCardiaque.cpp

--- CapteurCardiaque.cpp
+++ CapteurCardiaque.cpp
@@ -1,66 +1,91 @@
+#include <Arduino.h>
 #include "CapteurCardiaque.h"
 
-//using namespace std;
-//constructeur de CapteurCardiaque
+////////////////////////////////  constructeur de CapteurCardiaque  ////////////////////////////////////////////
+
 CapteurCardiaque::CapteurCardiaque(int pinIn,int pinOut){
+  DEAD=false;
+  pinEntree=pinIn;
+  pinSortie=pinOut;
+
   //Configuration du pin
-  pinMode(pinOut, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(pinIn), interrupt, RISING);
+  pinMode(pinSortie, OUTPUT);
   data_effect=true;
   Serial.println("Capteur OK!");
 
 }
+////////////////////////////////////  Assesseurs ///////////////////////////////////////////////////////////////
 
-void CapteurCardiaque::sendHeartbeat(){
-  Serial.println("the heart rate is "+heart_rate);
+int CapteurCardiaque::getPinEntree(){
+  return pinEntree;
 }
 
-void CapteurCardiaque::calculateHeartbeat(){
-  if(data_effect)
-    {
-      heart_rate=1200000/(temp[20]-temp[0]);//60*20*1000/20_total_time 
-      Serial.print("Heart_rate_is:\t");
-      Serial.println(heart_rate);
-    }
-    data_effect=1;//sign bit
-
-/*Function: Interrupt service routine.Get the sigal from the external interrupt*/
+int CapteurCardiaque::getPinSortie(){
+  return pinSortie;
 }
 
 int CapteurCardiaque::getHeartbeat(){
   return heart_rate;
 }
+bool CapteurCardiaque::getState(){
+  return DEAD;
+}
+//////////////////////////////////  Methodes ////////////////////////////////////////////////////////////
 
-void /*IRAM_ATTR*/ CapteurCardiaque::interrupt(){
+void CapteurCardiaque::sendHeartbeat(){
+  Serial.println("the heart rate is "+heart_rate);
+}
+
+void CapteurCardiaque::sum()
+    {
+     if(data_effect)
+        {
+          heart_rate=1200000/(temp[20]-temp[0]);//60*20*1000/20_total_time 
+          //Serial.print("Heart_rate_is:\t");
+          //Serial.println(heart_rate);
+        }
+       data_effect=1;//sign bit
+    }
+
+void CapteurCardiaque::arrayInit()
+{
+    for(unsigned char i=0;i < 20;i ++)
+    {
+        temp[i]=0;
+    }
+    temp[20]=millis();
+}
+
+void CapteurCardiaque::interrupt(){
   temp[counter]=millis();
   
-  /*Serial.println(counter,DEC);
-  Serial.println(temp[counter]);*/
+  //Serial.println(counter,DEC);
+ // Serial.println(temp[counter]);
 
   switch(counter)
   {
     case 0:
       sub=temp[counter]-temp[20];
-      Serial.println(sub);
+      //Serial.println(sub);
       break;
     default:
       sub=temp[counter]-temp[counter-1];
-      Serial.println(sub);
+      //Serial.println(sub);
       break;
   }
   if(sub>max_heartpluse_duty)//set 2 seconds as max heart pluse duty
   {
     data_effect=0;//sign bit
     counter=0;
-    Serial.println("Heart rate measure error,test will restart!" );
+    Serial.println("No heart rate detected,test will restart!" );
     DEAD=true;
     arrayInit();
-    digitalWrite(pinOut, HIGH);
+    digitalWrite(pinSortie, HIGH);
   }
   else
   {
-    DEAD=0;
-    digitalWrite(pinOut, LOW);
+    DEAD=false;
+    digitalWrite(pinSortie, LOW);
   }
   if (counter==20&&data_effect)
   {
@@ -76,3 +101,5 @@ void /*IRAM_ATTR*/ CapteurCardiaque::interrupt(){
       data_effect=1;
   }
 }
+
+ 
